fd0d1d9157ac70d374e1686ba678918659e4df28
Agathe Laburthe TOlra <agathe.laburthetolra@gmail.com>
2022-02-08 10:01:58 +0100

    feat(back): product can be added and can be show in his category

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,21 @@
 class ProductsController < ApplicationController
+  def new
+    @category = Category.find(params[:category_id])
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @category = Category.find(params[:category_id])
+    @product.category = @category
+    @product.save
+    redirect_to category_path(@category)
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :brand, :color, :price)
+  end
+
 end
